2e8cece1d074b8d4ad67c1f7b8ece148b434235e
Rhadow <howard3433@gmail.com>
2015-07-01 23:58:00 +0800

    added menu style

--- src/components/container/main/main.js
+++ src/components/container/main/main.js
@@ -4,6 +4,7 @@ import './_main';
 // Library
 import React from 'react';
 import {RouteHandler} from 'react-router';
+import classNames from 'classnames';
 
 // Components
 import BaseComponent from '../../base-component';
@@ -11,22 +12,45 @@ import BaseComponent from '../../base-component';
 class Main extends BaseComponent {
 	constructor(props) {
 		super(props);
+        this._bind(
+            '_getCurrentService'
+        );
 	}
+    _getCurrentService() {
+        return this.context.router.getCurrentPathname();
+    }
     render() {
+        let currentService = this._getCurrentService(),
+            homeClassNames = classNames({
+                'active': currentService === '/'
+            }),
+            sorterClassNames = classNames({
+                'active': currentService === '/name-sorter'
+            });
         return (
             <div className="main container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <ul>
-                            <li>
-                                <a href="#/name-sorter">Name Sorter</a>
-                            </li>
-                            <li>
-                                <a href="#/">Home</a>
-                            </li>
-                        </ul>
+                <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button
+                                type="button"
+                                className="navbar-toggle collapsed"
+                                data-toggle="collapse"
+                                data-target="#dissertation-helper-nav"
+                                aria-expanded="false">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div className="collapse navbar-collapse" id="dissertation-helper-nav">
+                            <ul className="nav navbar-nav">
+                                <li className={homeClassNames}><a href="#/">簡介</a></li>
+                                <li className={sorterClassNames}><a href="#/name-sorter">文獻排列小幫手</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                </nav>
                 <div className="row">
                     <div className="col-md-12">
                         <RouteHandler />
@@ -36,5 +60,9 @@ class Main extends BaseComponent {
         );
     }
 }
+
+Main.contextTypes = {
+    router: React.PropTypes.func.isRequired
+};
 
 export default Main;
